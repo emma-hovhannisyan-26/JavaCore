44116031c7ad95aefc79ae0839b569663d54af19
emma-hovhannisyan <emmahovhannisyan555@gmail.com>
2022-12-16 14:31:03 +0400

    JavaCore

---
+++ src/homework/AutoArray.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_day[] = {31, 28, 31, 30, 31, 30, 31, 31,
+                30, 31, 30, 31};
+
+        System.out.println(
+                "В апреле " + month_day[3] + "дней");
+    }
+}
